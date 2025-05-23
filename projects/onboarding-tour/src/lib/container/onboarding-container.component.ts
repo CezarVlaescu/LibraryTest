6cf4bf0e5389d512aa2ac54609cfe6219a426057
Vlaescu Cezar <vlaescucezar@yahoo.com>
2025-05-23 16:17:28 +0300

    last-update-May-23

--- projects/onboarding-tour/src/lib/container/onboarding-container.component.ts
+++ projects/onboarding-tour/src/lib/container/onboarding-container.component.ts
@@ -1,6 +1,7 @@
-import { Component, InputSignal, input } from '@angular/core';
+import { Component, InputSignal, OnInit, OutputEmitterRef, WritableSignal, input, output, signal } from '@angular/core';
 import { OnBoardingContainerConfig, OnBoardingSlideConfig } from '../models/on-boarding.model';
-import processedSlidesUtil from '../utils/on-boarding.util';
+import { processedSlidesUtil } from '../utils/on-boarding.util';
+import { environment } from '../../environments/onboarding.environment';
 
 @Component({
     selector: 'mc-onboarding-container',
@@ -8,10 +9,37 @@ import processedSlidesUtil from '../utils/on-boarding.util';
     styleUrls: ['./onboarding-container.component.css'],
     standalone: false
 })
-export class OnboardingContainerComponent {
+export class OnboardingContainerComponent implements OnInit {
   public containerConfig: InputSignal<OnBoardingContainerConfig> = input.required<OnBoardingContainerConfig>();
+  public tourClosed: OutputEmitterRef<void> = output<void>();
 
-  public get slidesSetup(): OnBoardingSlideConfig[] {
-    return processedSlidesUtil(this.containerConfig().slidesConfiguration);
+  public currentSlideIndex: WritableSignal<number> = signal<number>(0);
+  public slides: WritableSignal<OnBoardingSlideConfig[]> = signal<OnBoardingSlideConfig[]>([]);
+
+  public ngOnInit(): void {
+    this.slides.set(processedSlidesUtil(this.containerConfig().slidesConfiguration));
+
+    const hasSeenTour = localStorage.getItem(environment.onboardingTourKey);
+    if (hasSeenTour) {
+      this.currentSlideIndex.set(-1);
+    }
+  }
+
+  public handleStart(): void {
+    this.currentSlideIndex.set(1);
+  }
+
+  public handleNext(): void {
+    this.currentSlideIndex.update((prev) => Math.min(prev + 1, this.slides.length - 1));
+  }
+
+  public handleBack(): void {
+    this.currentSlideIndex.update((prev) => Math.max(prev - 1, 0));
+  }
+
+  public handleFinish(): void {
+    localStorage.setItem(environment.onboardingTourKey, 'true');
+    this.tourClosed.emit();
+    this.currentSlideIndex.set(-1);
   }
 }
